57363335edf1db8a9df751a7193ca8c07d8ac118
Leonid Konchulizov <leonid.konchulizov@tripster.ru>
2025-07-26 12:58:51 +0300

    feat: daysi ui

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,10 @@
-import {defineConfig, presetAttributify, presetIcons, presetWind4,} from 'unocss'
+import {defineConfig, presetAttributify, presetIcons, presetWind4} from 'unocss'
+import { presetDaisy } from "@ameinhardt/unocss-preset-daisy";
 
 export default defineConfig({
   presets: [
     presetAttributify(),
+    presetDaisy(),
     presetWind4(),
     presetIcons(),
   ],
